534af34475d41865b042242448444a6ca07cf91f
allanjoseph98 <allanjoseph98@gmail.com>
2022-12-03 14:32:11 +0530

    code golf

--- 2022/03/03.py
+++ 2022/03/03.py
@@ -6,15 +6,11 @@ with open('input.txt', 'r') as file:
 
 def priority(x: str) -> int: return ord(x)-64+26 if ord(x) <= 95 else ord(x)-96
 
-
-def mid(x: str) -> int: return int(len(x)/2)
-
-
 def cal(group) -> int:
     return priority(reduce(set.intersection, map(set, group)).pop())
 
 
-part1 = map(cal, tuple((rucksack[:mid(rucksack)], rucksack[mid(rucksack):]) for rucksack in rucksacks))
+part1 = map(cal, tuple((rucksack[:len(rucksack)//2], rucksack[len(rucksack)//2:]) for rucksack in rucksacks))
 part2 = map(cal, tuple((rucksacks[i], rucksacks[i+1], rucksacks[i+2]) for i in range(0,len(rucksacks),3)))
 
 print(sum(part1))
