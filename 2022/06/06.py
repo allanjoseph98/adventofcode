8a2ed061d9b7c1162a4a94726e40ca2dc2e758a2
allanjoseph98 <allanjoseph98@gmail.com>
2022-12-06 12:27:09 +0530

    break better

--- 2022/06/06.py
+++ 2022/06/06.py
@@ -8,7 +8,7 @@ for i in range(3, len(stream)):
     message = [stream[j] for j in range(i-13, i+1)]
     part1 = i+1 if (len(set(packets)) == 4 and not part1) else part1
     part2 = i+1 if (len(set(message)) == 14 and not part2) else part2
-    if part1 and part2:
+    if part2:
         break
 
 print(part1, part2)
